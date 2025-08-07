244195a0400868845fdda704383a3305471f7055
ojgonzalezz <ojgonzalezz27@gmail.com>
2025-08-07 16:00:15 -0300

    modificacion de title

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard Energy"
+app.title = "Dashboard energia"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
